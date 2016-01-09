2eb95ff39aa338286cbce847c845532e01a3b56c
Shing Lyu <shing.lyu@gmail.com>
2016-01-10 07:52:25 +0800

    Good 003

--- 003_Longest_Substring_Without_Repeating_Characters.cpp
+++ 003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,37 +1,39 @@
+/* 
+ * We have to helpers
+ * rightMostOccurancePos is a hashmap: char => its right most occurance position
+ * startPos is the starting point of the currently searching substring
+ * 
+ * Start from index 0, move idx along the string
+ * if found new char, put its latest (right most) occurance index into the map
+ * else if it's an existing one
+ *   check if the current substring (from startPos to current idx) is the largest, if yes => increase the global largest record
+ *   since we found an existing char, we "cut off" the old occurance of the char and start a new search from the right of the new string.
+ *   e.g. if we've seen "xyzabc", then we see a "xyzabca", we cut off the old "a" and start from "b"
+ *
+ *   Finally, the string may terminate so the last startPos to the end may be a longer string than the maxSize recorded, hence the last max()
+ *   
+ *     
+ */ 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxSize = 0;
+        map<char, int> rightMostOccurancePos;
+        int startPos = 0; // Start position of the currently longest substring
+
+        int maxSize = 0; 
         for (int idx = 0; idx < s.length(); ++idx){
-          for (int len = maxSize; len < s.length() - idx; ++len) {
-            
-            //cout << idx << len << s.substr(idx, len) << endl;
-            
-            if (this->_noRepeat(s.substr(idx, len))){
-              maxSize = len;
+          if (rightMostOccurancePos.find(s[idx]) == rightMostOccurancePos.end()){
+            rightMostOccurancePos.insert(pair<char, int>(s[idx], idx));
+          }
+          else {
+            if ((idx - startPos) >= maxSize){
+              maxSize = (idx - startPos);
             }
-            else {
-                break;
-                
-            }
+              startPos = max(rightMostOccurancePos[s[idx]] + 1, startPos);
+              rightMostOccurancePos[s[idx]] = idx;
           }
         }
-        return maxSize;
-    }
-private: 
-    bool _noRepeat(string s){  
-      map<char, int> contains;
-      for (string::iterator idx = s.begin(); idx != s.end(); idx++){ 
-        //cout << *idx << endl;
-        //cout << contains.find(idx) << endl;
-        map<char, int>::iterator it = contains.find(*idx);
-        if (it == contains.end()){
-            contains.insert(pair<char, int>(*idx, 1));
-        }
-        else {
-            return false;
-        }
-      }
-      return true;
+        int lastSubstringSize = s.length() - startPos;
+        return max(maxSize, lastSubstringSize);
     }
 };
